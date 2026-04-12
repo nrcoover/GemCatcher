024b5127267118c6cc5df39cb9238fe2db366a34
nicklefritz <nrcoover@gmail.com>
2026-04-12 09:08:44 -0400

    Add rotation variation to the gem movement

--- scripts/Gem.cs
+++ scripts/Gem.cs
@@ -5,21 +5,33 @@ public partial class Gem : Area2D
 	[Export] float _movementSpeed = 100;
 	[Export] float _minSpeedVariation = 1.0f;
 	[Export] float _maxSpeedVariation = 3.5f;
+	[Export] float _rotationSpeed = 15.0f;
+	[Export] float _minRotationVariation = -0.5f;
+	[Export] float _maxRotationVariation = 0.5f;
 	
 	private float _speedVariation;
+	private float _rotationVariation;
 
 	public override void _Ready()
 	{
 		_speedVariation = Helper.GetRandomFloat(_minSpeedVariation, _maxSpeedVariation);
+
+		_rotationVariation = Helper.GetRandomFloat(_minRotationVariation, _maxRotationVariation);
 	}
 
 	public override void _Process(double delta)
 	{
-		HandleGemMovement(delta);
+		HandleMovement((float)delta);
+		HandleRotation((float)delta);
 	}
 
-	private void HandleGemMovement(double delta)
+	private void HandleMovement(float delta)
 	{
 		Position = new Vector2(Position.X, Position.Y + (_movementSpeed * _speedVariation * (float)delta));
+	}
+
+	private void HandleRotation(float delta)
+	{
+		Rotation += _rotationSpeed * _rotationVariation * delta;
 	}
 }
