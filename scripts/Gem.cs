26db6c233215ce12a6fc0d31e58616ee39d41e07
nicklefritz <nrcoover@gmail.com>
2026-04-14 23:49:56 -0400

    Handle gem lifecycle in game
    Handle object cleanup when player scores and when gem is missed and goes outside of bounds.

--- scripts/Gem.cs
+++ scripts/Gem.cs
@@ -8,25 +8,38 @@ public partial class Gem : Area2D
 	[Export] float _rotationSpeed = 15.0f;
 	[Export] float _minRotationVariation = -0.5f;
 	[Export] float _maxRotationVariation = 0.5f;
+
+	[Export] private Paddle paddle;
 	
 	private float _speedVariation;
 	private float _rotationVariation;
 
 	public override void _Ready()
 	{
-		_speedVariation = Helper.GetRandomFloat(_minSpeedVariation, _maxSpeedVariation);
-
-		_rotationVariation = Helper.GetRandomFloat(_minRotationVariation, _maxRotationVariation);
+		SetVariations();
+		SubscribeToSignals();
 	}
 
 	public override void _Process(double delta)
 	{
 		HandleMovement((float)delta);
-		HandleRotation((float)delta);
 	}
 
-	private void HandleMovement(float delta)
+		private void HandleMovement(float delta)
 	{
+		HandlePosition(delta);
+		HandleRotation(delta);
+	}
+
+	private void HandlePosition(float delta)
+	{
+		var viewportBoundaryMargin = 75;
+
+		if (Position.Y > GetViewportRect().End.Y + viewportBoundaryMargin)
+		{
+			QueueFree();
+		}
+
 		Position = new Vector2(Position.X, Position.Y + (_movementSpeed * _speedVariation * (float)delta));
 	}
 
@@ -34,4 +47,28 @@ public partial class Gem : Area2D
 	{
 		Rotation += _rotationSpeed * _rotationVariation * delta;
 	}
+
+	private void SetVariations()
+	{
+		_speedVariation = Helper.GetRandomFloat(_minSpeedVariation, _maxSpeedVariation);
+
+		_rotationVariation = Helper.GetRandomFloat(_minRotationVariation, _maxRotationVariation);
+	}
+
+	private void SubscribeToSignals()
+	{
+		AreaEntered += OnAreaEntered;
+	}
+
+	#region Signals
+
+	private void OnAreaEntered(Area2D area)
+	{
+		if (area is Paddle)
+		{
+			QueueFree();
+		}
+	}
+
+	#endregion
 }
