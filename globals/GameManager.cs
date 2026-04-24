31acb89d517911f6cd9d86fbcd785f0ed83f118b
nicklefritz <nrcoover@gmail.com>
2026-04-23 23:38:53 -0400

    Manage missed gem count in game manager

--- globals/GameManager.cs
+++ globals/GameManager.cs
@@ -2,20 +2,14 @@ using Godot;
 
 public partial class GameManager : Node
 {
+	const int MAX_MISSED_GEMS = 3;
+
 	public static GameManager Instance {get; private set;}
 
 	private int _missedGemsCount = 0;
 
-	public int MissedGemsCount
-	{
-		get => _missedGemsCount;
-		set
-		{
-			_missedGemsCount = value;
-			GD.Print("Missed Gems: " + _missedGemsCount.ToString());
-		} 
-	}
-
+	public int MissedGemsCount = 0;
+	
 	public override void _Ready()
 	{
 		if (Instance == null)
@@ -30,6 +24,34 @@ public partial class GameManager : Node
 
 	private void ResetGame()
 	{
-		_missedGemsCount = 0;
+		SetMissedGemCount(0);
 	}
+
+#region Manage Gems
+
+	public int GetMissedGemCount()
+	{
+		return _missedGemsCount; 
+	}
+
+	public void SetMissedGemCount(int value)
+	{
+		_missedGemsCount = Mathf.Abs(value);
+	}
+
+	public void IncrementMissedGems()
+	{
+		_missedGemsCount ++;
+		GD.Print("Missed Gems: " + _missedGemsCount.ToString());
+
+		// TODO: Replace handling Game Over with health count
+		// if health count == 0, then the game is over
+		// This current method doesn't work if health can be restored.
+		if(MissedGemsCount == MAX_MISSED_GEMS)
+		{
+			GD.Print("Maximum Missed Gems Exceeded! GAME OVER!");
+		}
+	}
+
+#endregion
 }
