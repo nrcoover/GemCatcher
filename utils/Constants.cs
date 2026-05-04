bf6f4da573100648951b46d5f6fae6a7ff339f32
nicklefritz <nrcoover@gmail.com>
2026-05-03 22:54:29 -0400

    Add additional colors

--- utils/Constants.cs
+++ utils/Constants.cs
@@ -13,20 +13,24 @@ public static partial class Constants
   public static class CustomColors
   {
     // Pastelle Colors
-    public static readonly string RedPastelle = "F09EA7";
-    public static readonly string OrangePastelle = "F6CA94";
-    public static readonly string YellowPastelle = "FAFABE";
-    public static readonly string GreenPastelle = "C1EBC0";
-    public static readonly string BluePastelle = "C7CAFF";
-    public static readonly string PurplePastelle = "CDABEB";
-    public static readonly string PinkPastelle = "F6C2F3";
+    public static readonly string RedPastelle = "FF8888";
+    public static readonly string OrangePastelle = "FFCC88";
+    public static readonly string YellowPastelle = "FFFF88";
+    public static readonly string GreenPastelle = "88FF88";
+    public static readonly string BlueLightPastelle = "88FFFF";
+    public static readonly string BluePastelle = "88CCFF";
+    public static readonly string BlueDarkPastelle = "8888FF";
+    public static readonly string PurplePastelle = "CC88FF";
+    public static readonly string PinkPastelle = "FF88FF";
 
     // Bright Colors
     public static readonly string RedBright = "FF0000";
     public static readonly string OrangeBright = "FF8000";
     public static readonly string YellowBright = "FFFF00";
     public static readonly string GreenBright = "00FF00";
-    public static readonly string BlueBright = "0000FF";
+    public static readonly string BlueLightBright = "00FFFF";
+    public static readonly string BlueBright = "0080FF";
+    public static readonly string BlueDarkBright = "0000FF";
     public static readonly string PurpleBright = "8000FF";
     public static readonly string PinkBright = "FF00FF";
 
@@ -35,7 +39,9 @@ public static partial class Constants
     public static readonly string OrangeMuted = "AA7755";
     public static readonly string YellowMuted = "AAAA55";
     public static readonly string GreenMuted = "55AA55";
-    public static readonly string BlueMuted = "5555AA";
+    public static readonly string BlueLightMuted = "55AAAA";
+    public static readonly string BlueMuted = "5577AA";
+    public static readonly string BlueDarkMuted = "5555AA";
     public static readonly string PurpleMuted = "7755AA";
     public static readonly string PinkMuted = "AA55AA";
   }
--- utils/Helper.cs
+++ utils/Helper.cs
@@ -24,38 +24,46 @@ public static partial class Helper
 
 	public static Color GetColorFromRainbow()
 	{
-		string selectedColorhex = Constants.CustomColors.RedBright;
+		string selectedColorhex = Constants.CustomColors.RedPastelle;
 
-		var randomNumber = GetRandomInt(0, 7) % 7;
+		var randomNumber = GetRandomInt(0, 9) % 9;
 
 		switch (randomNumber)
 		{
 			case 0:
-				selectedColorhex = Constants.CustomColors.RedBright;
+				selectedColorhex = Constants.CustomColors.RedPastelle;
 				break;
 			
 			case 1:
-				selectedColorhex = Constants.CustomColors.OrangeBright;
+				selectedColorhex = Constants.CustomColors.OrangePastelle;
 				break;
 
 			case 2:
-				selectedColorhex = Constants.CustomColors.YellowBright;
+				selectedColorhex = Constants.CustomColors.YellowPastelle;
 				break;
 
 			case 3:
-				selectedColorhex = Constants.CustomColors.GreenBright;
+				selectedColorhex = Constants.CustomColors.GreenPastelle;
 				break;
 
 			case 4:
-				selectedColorhex = Constants.CustomColors.BlueBright;
+				selectedColorhex = Constants.CustomColors.BlueLightPastelle;
 				break;
 
 			case 5:
-				selectedColorhex = Constants.CustomColors.PurpleBright;
+				selectedColorhex = Constants.CustomColors.BluePastelle;
 				break;
 
 			case 6:
-				selectedColorhex = Constants.CustomColors.PinkBright;
+				selectedColorhex = Constants.CustomColors.BlueDarkPastelle;
+				break;
+
+			case 7:
+				selectedColorhex = Constants.CustomColors.PurplePastelle;
+				break;
+
+			case 8:
+				selectedColorhex = Constants.CustomColors.PinkPastelle;
 				break;
 		}
 
