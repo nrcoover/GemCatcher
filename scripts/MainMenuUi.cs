9fea48c0e4bc88b313229357c0054b6a5fc994ba
nicklefritz <nrcoover@gmail.com>
2026-05-11 23:14:53 -0400

    Update menu title

--- scripts/MainMenuUi.cs
+++ scripts/MainMenuUi.cs
@@ -2,13 +2,31 @@ using Godot;
 
 public partial class MainMenuUi : Control
 {
-	[Export] TextureButton PlayButton;
-	[Export] TextureButton QuitButton;
+	[Export] TextureButton _playButton;
+	[Export] TextureButton _quitButton;
+	[Export] Label _titleLayer1;
+	[Export] Label _titleLayer2;
+	[Export] Label _titleLayer3;
+	[Export] Label _titleLayer4;
+	[Export] Label _titleLayer5;
+	[Export] Label _titleLayer6;
 
 	public override void _Ready()
 	{
-		PlayButton.Pressed += OnPlayClicked;
-		QuitButton.Pressed += OnQuitClicked;
+		_playButton.Pressed += OnPlayClicked;
+		_quitButton.Pressed += OnQuitClicked;
+
+		InitializeTitleColors();
+	}
+
+	private void InitializeTitleColors()
+	{
+		_titleLayer1.Modulate = new Color(Constants.CustomColors.PurpleBright);
+		_titleLayer2.Modulate = Color.FromString(Constants.CustomColors.BlueBright, Colors.White);
+		_titleLayer3.Modulate = Color.FromString(Constants.CustomColors.GreenBright, Colors.White);
+		_titleLayer4.Modulate = Color.FromString(Constants.CustomColors.YellowBright, Colors.White);
+		_titleLayer5.Modulate = Color.FromString(Constants.CustomColors.OrangeBright, Colors.White);
+		_titleLayer6.Modulate = Color.FromString(Constants.CustomColors.RedBright, Colors.White);
 	}
 
   private void OnPlayClicked()
