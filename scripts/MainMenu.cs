b035da93a8a6a53611fe7f193353bdfaa1e4b6a4
nicklefritz <nrcoover@gmail.com>
2026-05-12 17:00:02 -0400

    Polish paddle scene for main menu

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -3,6 +3,7 @@ using Godot;
 public partial class MainMenu : Node2D
 {
 	[Export] PackedScene Paddle {get; set;}
+	[Export] float _paddleScale = 0.75f; 
 
 	public override void _Ready()
 	{
@@ -13,5 +14,6 @@ public partial class MainMenu : Node2D
 	{
 		var paddle = (MenuPaddle)Paddle.Instantiate();
 		AddChild(paddle);
+		paddle.Scale = new Vector2(_paddleScale, _paddleScale);
 	}
 }
