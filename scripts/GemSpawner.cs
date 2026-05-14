0172b2c7b5e5beda197411de2c805d919591b7ce
nicklefritz <nrcoover@gmail.com>
2026-05-13 21:24:08 -0400

    Constrain gem spawning position on Main Menu

--- scripts/GemSpawner.cs
+++ scripts/GemSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 public partial class GemSpawner : Node2D
@@ -5,14 +6,30 @@ public partial class GemSpawner : Node2D
 	[Export] private Timer _gemSpawnTimer;
 	[Export] private PackedScene _gemScene;
 	[Export] private Node _gemContainer;
+	[Export] private bool _isOnMainMenu;
+
+	private Marker2D _leftBoundary;
+	private Marker2D _rightBoundary;
 
 	public override void _Ready()
 	{
+		if (_isOnMainMenu)
+		{
+			SetBoundaryMarkers();
+		}
+
 		SpawnGem();
 		SubscribeToSignals();
 	}
 
-	private void SubscribeToSignals()
+  private void SetBoundaryMarkers()
+  {
+    _leftBoundary = (Marker2D)GetNode("../../MainMenu/CanvasLayer/MainMenuUi/MarginContainer/TitleContainer/LeftBoundary");
+
+		_rightBoundary = (Marker2D)GetNode("../../MainMenu/CanvasLayer/MainMenuUi/MarginContainer/TitleContainer/RightBoundary");
+  }
+
+  private void SubscribeToSignals()
 	{
 		_gemSpawnTimer.Timeout += SpawnGem;
 	}
@@ -22,6 +39,18 @@ public partial class GemSpawner : Node2D
 		var gem = (Gem)_gemScene.Instantiate();
 		_gemContainer.AddChild(gem);
 
+		if (_isOnMainMenu)
+		{
+			SetGemPositionOnMainMenu(gem);
+
+			return;
+		}
+		
+		SetGemPosition(gem);
+	}
+
+	private void SetGemPosition(Gem gem)
+	{
 		var margin = 85;
 
 		var xBoundaryCoordinate = Helper.GetRandomFloat(
@@ -32,6 +61,26 @@ public partial class GemSpawner : Node2D
 		gem.Position = new Vector2(
 				xBoundaryCoordinate,
 				-margin
+			);
+	}
+
+	private void SetGemPositionOnMainMenu(Gem gem)
+	{
+		if (_leftBoundary == null || _rightBoundary == null)
+		{
+			GD.Print("Marker Node Path Not Found!");
+			SetGemPosition(gem);
+			return;
+		}
+
+		var xBoundaryCoordinate = Helper.GetRandomFloat(
+				_rightBoundary.GlobalPosition.X,
+				_leftBoundary.GlobalPosition.X 
+			);
+
+		gem.Position = new Vector2(
+				xBoundaryCoordinate,
+				_leftBoundary.GlobalPosition.Y
 			);
 	}
 }
