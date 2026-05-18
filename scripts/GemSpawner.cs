086936ed4ca22e4cac1ad70bccc5542a222bb59e
nicklefritz <nrcoover@gmail.com>
2026-05-17 21:41:52 -0400

    Revert "Attempt to have gems filter down through different layers of the title"
    This reverts commit b009558b914280e5f86f2068d4cd70ae679ac2de.

--- scripts/GemSpawner.cs
+++ scripts/GemSpawner.cs
@@ -1,19 +1,12 @@
+using System;
 using Godot;
 
 public partial class GemSpawner : Node2D
 {
 	[Export] private Timer _gemSpawnTimer;
 	[Export] private PackedScene _gemScene;
-	[Export] private PackedScene _uiGemScene;
 	[Export] private Node _gemContainer;
 	[Export] private bool _isOnMainMenu;
-
-	[Export] private Node2D _layer1;
-	[Export] private Node2D _layer2;
-	[Export] private Node2D _layer3;
-	[Export] private Node2D _layer4;
-	[Export] private Node2D _layer5;
-	[Export] private Node2D _layer6;
 
 	private Marker2D _leftBoundary;
 	private Marker2D _rightBoundary;
@@ -44,20 +37,19 @@ public partial class GemSpawner : Node2D
 	private void SpawnGem()
 	{
 		var gem = (Gem)_gemScene.Instantiate();
-		
+		_gemContainer.AddChild(gem);
+
 		if (_isOnMainMenu)
 		{
 			SetGemPositionOnMainMenu(gem);
 
 			return;
 		}
-
-		_gemContainer.AddChild(gem);
 		
 		SetGemPosition(gem);
 	}
 
-  private void SetGemPosition(Gem gem)
+	private void SetGemPosition(Gem gem)
 	{
 		var margin = 85;
 
@@ -81,8 +73,6 @@ public partial class GemSpawner : Node2D
 			return;
 		}
 
-		SetZIndexOnMainMenu(gem);
-
 		var xBoundaryCoordinate = Helper.GetRandomFloat(
 				_rightBoundary.GlobalPosition.X,
 				_leftBoundary.GlobalPosition.X 
@@ -92,66 +82,5 @@ public partial class GemSpawner : Node2D
 				xBoundaryCoordinate,
 				_leftBoundary.GlobalPosition.Y
 			);
-	}
-
-	private void SetZIndexOnMainMenu(Gem gem)
-	{
-		var color = gem.Modulate;
-
-		// gem.ZIndex = Helper.SetZIndexByColor(gem.Modulate);
-
-		// GD.Print($"COLOR: {color}");
-
-		var redPastell = new Color (Constants.CustomColors.RedPastelle);
-		var orangePastelle = new Color (Constants.CustomColors.OrangePastelle);
-		var yellowPastelle = new Color (Constants.CustomColors.YellowPastelle);
-		var greenPastelle = new Color (Constants.CustomColors.GreenPastelle);
-		var blueLightPastelle = new Color (Constants.CustomColors.BlueLightPastelle);
-		var bluePastelle = new Color (Constants.CustomColors.BluePastelle);
-		var blueDarkPastelle = new Color (Constants.CustomColors.BlueDarkPastelle);
-		var purplePastelle = new Color (Constants.CustomColors.PurplePastelle);
-		var pinkPastelle = new Color (Constants.CustomColors.PinkPastelle);
-
-		if (color == redPastell)
-		{
-			GD.Print("RED DISCOVERED!");
-			_layer6.AddChild(gem);
-		}
-		else if (color == orangePastelle)
-		{
-			_layer5.AddChild(gem);
-		}
-		else if (color == yellowPastelle)
-		{
-			_layer4.AddChild(gem);
-		}
-		else if (color == greenPastelle)
-		{
-			_layer3.AddChild(gem);
-		}
-		else if (color == blueLightPastelle)
-		{
-			_layer2.AddChild(gem);
-		}
-		else if (color == bluePastelle)
-		{
-			_layer2.AddChild(gem);
-		}
-		else if (color == blueDarkPastelle)
-		{
-			_layer2.AddChild(gem);
-		}
-		else if (color == purplePastelle)
-		{
-			_layer1.AddChild(gem);
-		}
-		else if (color == pinkPastelle)
-		{
-			_gemContainer.AddChild(gem);
-		}
-		else
-		{
-			_gemContainer.AddChild(gem);
-		}
 	}
 }
