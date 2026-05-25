0eff8db3dc24c10498285368e09c177075de673d
nicklefritz <nrcoover@gmail.com>
2026-05-25 09:41:58 -0400

    Fix bug preventing update of high score restore value

--- globals/ScoreManager.cs
+++ globals/ScoreManager.cs
@@ -19,6 +19,12 @@ public partial class ScoreManager : Node
 		set
 		{
 			_highScore = Mathf.Max(0, value);
+
+			if (value > HighScoreRestore)
+			{
+				HighScoreRestore = value;
+			}
+			
 			SaveScoreToFile();
 		}
 	}
